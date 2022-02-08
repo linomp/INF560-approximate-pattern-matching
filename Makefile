cbbde10b7033d72ef49e4ed1713470a031e40b0a
linomp <lm2p@hotmail.es>
2022-02-08 18:05:43 +0100

    moved utility functions to separate header & added rule in makefile for apm with mpi approach # 1

--- Makefile
+++ Makefile
@@ -6,11 +6,13 @@ CC=gcc
 CFLAGS=-O3 -I$(HEADER_DIR) -Wall
 LDFLAGS=
 
-SRC= apm.c
+SRC= apm.c apm_mpi_patterns_over_ranks.c
 
 OBJ= $(OBJ_DIR)/apm.o
 
-all: $(OBJ_DIR) apm
+OBJ_MPI_1= $(OBJ_DIR)/apm_mpi_patterns_over_ranks.o
+
+all: $(OBJ_DIR) apm apm_mpi_patterns_over_ranks
 
 $(OBJ_DIR):
 	mkdir $(OBJ_DIR)
@@ -21,5 +23,8 @@ $(OBJ_DIR)/%.o : $(SRC_DIR)/%.c
 apm:$(OBJ)
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^
 
+apm_mpi_patterns_over_ranks:$(OBJ_MPI_1)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^
+
 clean:
-	rm -f apm $(OBJ) ; rmdir $(OBJ_DIR)
+	rm -f apm apm_mpi_patterns_over_ranks $(OBJ) $(OBJ_MPI_1) ; rmdir $(OBJ_DIR)
--- src/apm_mpi_patterns_over_ranks.c
+++ src/apm_mpi_patterns_over_ranks.c
@@ -4,8 +4,13 @@
  * INF560
  *
  * Approach #1: distribute the patterns over the MPI ranks
- * 
+ *
+ *
+ * Usage:
+ * ./apm.exe 0 dna/small_chrY.fa $(cat dna/line_chrY.fa)
+ *
  */
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,98 +18,11 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-#define APM_DEBUG 0
+#include "utils.h"
 
-char *
-read_input_file(char *filename, int *size)
-{
-    char *buf;
-    off_t fsize;
-    int fd = 0;
-    int n_bytes = 1;
+char *read_input_file(char *filename, int *size);
 
-    /* Open the text file */
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        fprintf(stderr, "Unable to open the text file <%s>\n", filename);
-        return NULL;
-    }
-
-    /* Get the number of characters in the textfile */
-    fsize = lseek(fd, 0, SEEK_END);
-    if (fsize == -1)
-    {
-        fprintf(stderr, "Unable to lseek to the end\n");
-        return NULL;
-    }
-
-#if APM_DEBUG
-    printf("File length: %lld\n", fsize);
-#endif
-
-    /* Go back to the beginning of the input file */
-    if (lseek(fd, 0, SEEK_SET) == -1)
-    {
-        fprintf(stderr, "Unable to lseek to start\n");
-        return NULL;
-    }
-
-    /* Allocate data to copy the target text */
-    buf = (char *)malloc(fsize * sizeof(char));
-    if (buf == NULL)
-    {
-        fprintf(stderr, "Unable to allocate %lld byte(s) for main array\n",
-                fsize);
-        return NULL;
-    }
-
-    n_bytes = read(fd, buf, fsize);
-    if (n_bytes != fsize)
-    {
-        fprintf(stderr,
-                "Unable to copy %lld byte(s) from text file (%d byte(s) copied)\n",
-                fsize, n_bytes);
-        return NULL;
-    }
-
-#if APM_DEBUG
-    printf("Number of read bytes: %d\n", n_bytes);
-#endif
-
-    *size = n_bytes;
-
-    close(fd);
-
-    return buf;
-}
-
-#define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
-
-int levenshtein(char *s1, char *s2, int len, int *column)
-{
-    unsigned int x, y, lastdiag, olddiag;
-
-    for (y = 1; y <= len; y++)
-    {
-        column[y] = y;
-    }
-    for (x = 1; x <= len; x++)
-    {
-        column[0] = x;
-        lastdiag = x - 1;
-        for (y = 1; y <= len; y++)
-        {
-            olddiag = column[y];
-            column[y] = MIN3(
-                column[y] + 1,
-                column[y - 1] + 1,
-                lastdiag + (s1[y - 1] == s2[x - 1] ? 0 : 1));
-            lastdiag = olddiag;
-        }
-    }
-    return (column[len]);
-}
+int levenshtein(char *s1, char *s2, int len, int *column);
 
 int main(int argc, char **argv)
 {
@@ -189,8 +107,8 @@ int main(int argc, char **argv)
     }
 
     /*****
-   * BEGIN MAIN LOOP
-   ******/
+     * BEGIN MAIN LOOP
+     ******/
 
     /* Timer start */
     gettimeofday(&t1, NULL);
@@ -250,8 +168,8 @@ int main(int argc, char **argv)
     printf("APM done in %lf s\n", duration);
 
     /*****
-   * END MAIN LOOP
-   ******/
+     * END MAIN LOOP
+     ******/
 
     for (i = 0; i < nb_patterns; i++)
     {
